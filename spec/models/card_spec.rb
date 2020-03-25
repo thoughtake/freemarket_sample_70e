1feebe8ec22453d711a4b868e26ef645fb703a7d
yuigawa <57977700+yuigawa@users.noreply.github.com>
2020-03-25 16:35:28 +0900

    commit

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-#クレカが登録してあれば購入できる
-describe  Card do
-  describe '#delete' do
-
-    it "is invalid without a customer_id" do
-      card = build(:card, customer_id: "")
-      card.valid?
-      expect(card.errors[:customer_id]).to include("can't be blank")
-    end
-
-    it "is invalid without an card_id" do
-      card = build(:card, card_id: "")
-      card.valid?
-      expect(card.errors[:card_id]).to include("can't be blank")
-    end
-  end
+RSpec.describe Card, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
 end
